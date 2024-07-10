4fabca75f1416fbf0c3e313187dbf338e9fe4261
atapasha <atapashazade@gmail.com>
2024-07-10 14:01:27 +0330

    complate signup page ui

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,40 @@
-import React from "react";
-
+import React, { useDebugValue } from "react";
+import { Link } from "react-router-dom";
 export default function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <div className="max-w-lg p-3 mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">SignUp</h1>
+      <form action="" className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="Username"
+          id="username"
+          className="p-3 rounded-lg bg-slate-100"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="p-3 rounded-lg bg-slate-100"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="p-3 rounded-lg bg-slate-100"
+        />
+
+        <button className="p-3 text-white uppercase rounded-lg bg-slate-700 hover:opacity-95 disabled:opacity-80 ">
+          Sign up
+        </button>
+      </form>
+
+      <div className="flex gap-2 mt-5">
+        <p>Have an Account?</p>
+        <Link to="/sign-in">
+          <span className="text-blue-600"> Sign in</span>
+        </Link>
+      </div>
+    </div>
+  );
 }
